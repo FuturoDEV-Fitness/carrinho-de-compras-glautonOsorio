16fa82680aad2f949901580611d31df299577ecd
Glauton Osório <glautonleao@gmail.com>
2024-07-02 08:08:10 -0300

    feat: rota de listar produtos finalizada

--- src/controllers/ProductsController/index.js
+++ src/controllers/ProductsController/index.js
@@ -1,8 +1,12 @@
+const { findAllProducts } = require("./function/product.index");
 const { createProduct } = require("./function/products.store");
 
 class ProductsController {
   async store(req, res) {
     await createProduct(req, res);
+  }
+  async index(req, res) {
+    await findAllProducts(req, res);
   }
 }
 
--- src/routes/v1/product.routes.js
+++ src/routes/v1/product.routes.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const ProductsController = require("../../controllers/ProductsController");
 
 router.post("/", ProductsController.store);
+router.get("/", ProductsController.index);
 
 module.exports = router;
