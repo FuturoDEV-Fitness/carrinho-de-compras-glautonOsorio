6c4cc0add1512c62e218ad9c88c52b41f9468fb2
Glauton Osório <glautonleao@gmail.com>
2024-07-08 05:50:54 -0300

    feat: adicionando funções extras no meu clientController

--- src/routes/v1/client.routes.js
+++ src/routes/v1/client.routes.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 
 const ClientsController = require("../../controllers/ClientsController");
 
+router.post("/", ClientsController.index);
+router.post("/:id", ClientsController.index);
 router.post("/", ClientsController.store);
+router.post("/:id", ClientsController.update);
+router.post("/:id", ClientsController.destroy);
 
 module.exports = router;
